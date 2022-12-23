16598841abfa125d4e8854dd68b66cd3fe2fc754
PrathmeshR <rathodprathmesh18@gmail.com>
2022-12-23 12:38:43 +0530

    Create SumOfArray.cpp

---
+++ Array/SumOfArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+// Driver Code
+int main()
+{
+    string name = "Akshay";
+    int age = 18;
+ 
+    // Print multiple variable on
+    // screen using cout
+    cout << "Name : " << name << endl
+         << "Age : " << age << endl;
+ 
+    return 0;
+    //result
+    //result
+}
